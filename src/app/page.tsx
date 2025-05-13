1ca9677a36bd71188ac16e9abd2c7464d7bb58f5
Joel Thompson <joel.thompson@circleci.com>
2025-05-13 10:16:41 -0700

    adding clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Counter } from "@/components/Counter";
+import { SignedIn } from "@clerk/nextjs";
+import { SignedOut } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-4">
       <h1 className="text-3xl font-bold">Hello World</h1>
       <Counter />
+      <SignedOut>You are not signed in</SignedOut>
+      <SignedIn>You are signed in</SignedIn>
     </div>
   );
 }
