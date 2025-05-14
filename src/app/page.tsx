a18691fe414bf2386998f3c4038220ccb9d36e9d
Joel Thompson <joel.thompson@circleci.com>
2025-05-14 09:02:02 -0700

    add some server examples

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { Counter } from "@/components/Counter";
 import { SignedIn } from "@clerk/nextjs";
 import { SignedOut } from "@clerk/nextjs";
+import { ServerActionExample } from "@/components/ServerActionExample";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-4">
       <h1 className="text-3xl font-bold">Hello World</h1>
-      <Counter />
       <SignedOut>You are not signed in</SignedOut>
       <SignedIn>You are signed in</SignedIn>
+      <Counter />
+      <ServerActionExample />
     </div>
   );
 }
